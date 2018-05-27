15d63e4fabc7947c392c0fe1d5aff3d6a8ba2187
Iran Gregorio <iran.gregorio@hotmail.com.br>
2018-05-26 21:17:19 -0300

    update  tests

--- tests/webscrapingtest.py
+++ tests/webscrapingtest.py
@@ -1,14 +1,23 @@
 import unittest
 import os
 import sys
+import requests
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'src'))
 from src import process
+from src.element import Element
 from bs4 import BeautifulSoup
 
 class Test(unittest.TestCase):
     def test_get_soup(self):
         self.assertIsInstance(process.get_soup("https://github.com/"), BeautifulSoup)
+
+    def test_get_file_info(self):
+        soup = process.get_soup("https://github.com/iran-gregorio/GitHubWebScraping/blob/master/README.md")
+        element = Element("", "", 0)
+        process.get_fileInfo(soup, element)
+        self.assertGreater(element.lines, 0)
+        self.assertGreater(element.size, 0)
 
 if __name__ == '__main__':
     unittest.main()
